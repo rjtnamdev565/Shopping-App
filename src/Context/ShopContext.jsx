ad9d7045229cd00979314b10bd02f9af3b14b373
Rajat_Namdev <rjtnamdev565@gmail.com>
2023-12-04 10:52:11 +0530

    first commit

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,53 @@
+import React, { createContext, useEffect, useState } from "react";
+import all_product from "../Components/Assets/all_product"
+import products from "../Components/Assets/products";
+export const ShopContext = createContext(null);
+const getDefaultCart = () => {
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+      cart[i] = 0;
+    }
+    return cart;
+  };
+const ShopContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState(getDefaultCart());
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+          if (cartItems[item] > 0) {
+            let itemInfo = products.find((product) => product.id === Number(item));
+            totalAmount += cartItems[item] * itemInfo.new_price;
+          }
+        }
+        return totalAmount;
+      };
+    
+      const getTotalCartItems = () => {
+        let totalItem = 0;
+        for (const item in cartItems) {
+          if (cartItems[item] > 0) {
+            totalItem += cartItems[item];;
+          }
+        }
+        return totalItem;
+      };
+    
+      const addToCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+      };
+    
+      const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+     
+      };
+    
+      const contextValue = {products, getTotalCartItems, cartItems, addToCart, removeFromCart, getTotalCartAmount };
+
+    return (
+        <ShopContext.Provider value={contextValue}>
+          {props.children}
+        </ShopContext.Provider>
+      );
+}
+export default ShopContextProvider;
---
+++ src/Pages/ShopCategory.jsx
@@ -0,0 +1,35 @@
+import React, { useContext, useEffect, useState } from "react";
+import "./CSS/ShopCategory.css";
+import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import Item from "../Components/Item/Item";
+import { Link } from "react-router-dom";
+import { ShopContext } from "../Context/ShopContext";
+const ShopCategory = (props) => {
+const {products} = useContext(ShopContext)
+  return (
+    <div className="shopcategory">
+      <img src={props.banner} className="shopcategory-banner" alt="" />
+      <div className="shopcategory-indexSort">
+        <p><span>Showing 1 - 12</span> out of 54 Products</p>
+        <div className="shopcategory-sort">Sort by  <img src={dropdown_icon} alt="" /></div>
+      </div>
+      <div className="shopcategory-products">
+        {products.map((item,i) => {
+            if(props.category===item.category)
+            {
+              return <Item id={item.id} key={i} name={item.name} image={item.image}  new_price={item.new_price} old_price={item.old_price}/>;
+            }
+            else
+            {
+              return null;
+            }
+        })}
+      </div>
+      <div className="shopcategory-loadmore">
+      <Link to='/' style={{ textDecoration: 'none' }}>Explore More</Link>
+      </div>
+    </div>
+  );
+};
+
+export default ShopCategory;
